2ab31a1be3e83ba9156fa4f3249f80ab466d78cb
siqube <reich.davidr@gmail.com>
2025-12-01 08:28:51 +0100

    initial commit

---
+++ day00/part1.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+import os.path
+
+import pytest
+
+
+_INPUT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def _solve(inp: str) -> int:
+    result = 0
+
+    return result
+
+
+_TESTS = """\
+"""
+_EXPECTED = 0
+
+
+@pytest.mark.parametrize(
+    ('input_str', 'expected'),
+    (
+        (_TESTS, _EXPECTED),
+    ),
+)
+def test(input_str: str, expected: int) -> None:
+    assert _solve(input_str) == expected
+
+
+def main() -> int:
+    with open(_INPUT) as fp:
+        print(_solve(fp.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
